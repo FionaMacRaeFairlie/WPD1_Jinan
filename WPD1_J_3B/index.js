bfd0f719d31e7adcaa65dea4d73c9f37c74d604f
FionaMacRaeFairlie <88429943+FionaMacRaeFairlie@users.noreply.github.com>
2023-11-02 11:22:20 +0000

    updated nav style

--- WPD1_J_3B/index.js
+++ WPD1_J_3B/index.js
@@ -6,7 +6,6 @@ app.use(express.static(public));
 
 app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js')); // redirect bootstrap JS
 app.use('/js', express.static(__dirname + '/node_modules/jquery/dist')); // redirect JS jQuery
-app.use('/js', express.static(__dirname + '/node_modules/@popper/core/dist/umd')); // redirect JS jQuery
 app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css')); // redirect CSS bootstrap
 
 app.get("/", function (req, res) {
--- WPD1_J_5A/index.js
+++ WPD1_J_5A/index.js
@@ -10,6 +10,8 @@ let counter = 0;
 
 app.use(express.static(public));
 app.use("/css", express.static(__dirname + "/node_modules/bootstrap/dist/css")); // redirect CSS bootstrap
+app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js')); // redirect bootstrap JS
+app.use('/js', express.static(__dirname + '/node_modules/jquery/dist')); // redirect JS jQuery
 
 app.use(express.urlencoded({ extended: false }));
 
